cbdede47c88eb33bb9d9165c22a8843257d47afa
prasad <prasad@gmail.com>
2025-07-19 17:47:07 +0530

    adding code-3

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,11 +5,12 @@ var express = require('express');
 var app = module.exports = express();
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send('Hello World, How are you');
 });
 
 /* istanbul ignore next */
 if (!module.parent) {
   app.listen(8080);
   console.log('Express started on port 8080');
+  console.log('Hello good morning');
 }
