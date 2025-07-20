8a2f85e90fd4345da84da32b4789f84f46df51c6
prasad <prasad@gmail.com>
2025-07-20 19:23:54 +0530

    adding code-8

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = module.exports = express();
 
 app.get('/', function(req, res){
-  res.send('Hello World from version 5 ');
+  res.send('Hello World from version 6 ');
 });
 
 /* istanbul ignore next */
